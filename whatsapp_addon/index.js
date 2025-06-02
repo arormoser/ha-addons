0ca83ff39e9cc08ddf3418c1a8a5d28b1c80698c
arormoser <arormoser@gmail.com>
2025-06-02 18:40:07 -0300

    Update index.js

--- whatsapp_addon/index.js
+++ whatsapp_addon/index.js
@@ -1,258 +1,28 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const axios = require("axios");
-const fs = require("fs");
-const { WhatsappClient } = require("./whatsapp");
-
-var logger = require("log4js").getLogger();
-logger.level = "info";
-console.log('INDEX VERSION:', Date.now());
-console.log('BAILEYS VERSION:', require('@whiskeysockets/baileys/package.json').version);
-console.log('NODE VERSION:', process.version);
-
-var qrimage = require("qr-image");
+const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const clients = {};
+const LOCAL_NODE_URL = "http://frigate.local:3000/sendMessage"; // Cambia esto por la IP de tu server Node
 
-const onReady = (key) => {
-  logger.info(key, "client is ready.");
-  axios.post(
-    "http://supervisor/core/api/services/persistent_notification/dismiss",
-    {
-      notification_id: `whatsapp_addon_qrcode_${key}`,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.SUPERVISOR_TOKEN}`,
-      },
-    }
-  );
-};
+app.post("/sendMessage", async (req, res) => {
+  try {
+    // Opcional: loguea el request
+    console.log("Proxy recibiendo:", req.body);
 
-const onQr = (qr, key) => {
-  logger.info(
-    key,
-    "require authentication over QRCode, please see your notifications..."
-  );
+    // Reenvía a tu Node
+    const r = await axios.post(LOCAL_NODE_URL, req.body);
 
-  var code = qrimage.image(qr, { type: "png" });
+    // Devuelve el resultado a HA
+    res.status(r.status).send(r.data);
+  } catch (err) {
+    console.error("Proxy error:", err.message);
+    res.status(500).send("Error en el proxy: " + err.message);
+  }
+});
 
-  code.on("readable", function () {
-    var img_string = code.read().toString("base64");
-    axios.post(
-      "http://supervisor/core/api/services/persistent_notification/create",
-      {
-        title: `Whatsapp QRCode (${key})`,
-        message: `Please scan the following QRCode for **${key}** client... ![QRCode](data:image/png;base64,${img_string})`,
-        notification_id: `whatsapp_addon_qrcode_${key}`,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.SUPERVISOR_TOKEN}`,
-        },
-      }
-    );
-  });
-};
-
-const onMsg = (msg, key) => {
-  axios.post(
-    "http://supervisor/core/api/events/new_whatsapp_message",
-    { clientId: key, ...msg },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.SUPERVISOR_TOKEN}`,
-      },
-    }
-  );
-  logger.debug(`New message event fired from ${key}.`);
-};
-
-const onPresenceUpdate = (presence, key) => {
-  axios.post(
-    "http://supervisor/core/api/events/whatsapp_presence_update",
-    { clientId: key, ...presence },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.SUPERVISOR_TOKEN}`,
-      },
-    }
-  );
-  logger.debug(`New presence event fired from ${key}.`);
-};
-
-const onLogout = async (key) => {
-  logger.info(`Client ${key} was logged out. Restarting...`);
-  fs.rm(`/data/${key}`, { recursive: true });
-
-  init(key);
-};
-
-const init = (key) => {
-  clients[key] = new WhatsappClient({ path: `/data/${key}` });
-
-  clients[key].on("restart", () => logger.debug(`${key} client restarting...`));
-  clients[key].on("qr", (qr) => onQr(qr, key));
-  clients[key].once("ready", () => onReady(key));
-  clients[key].on("msg", (msg) => onMsg(msg, key));
-  clients[key].on("logout", () => onLogout(key));
-  clients[key].on("presence_update", (presence) =>
-    onPresenceUpdate(presence, key)
-  );
-};
-
-fs.readFile("data/options.json", function (error, content) {
-  var options = JSON.parse(content);
-
-  options.clients.forEach((key) => {
-    init(key);
-  });
-
-  app.listen(port, () => logger.info(`Whatsapp Addon started.`));
-
-  app.post("/sendMessage", (req, res) => {
-    const message = req.body;
-    if (message.hasOwnProperty("clientId")) {
-      if (clients.hasOwnProperty(message.clientId)) {
-        const wapp = clients[message.clientId];
-        logger.info("Entro a sendMessage 21");
-        logger.info("to:", message.to);
-        logger.info("body:", message.body);
-        logger.info("options:", message.options);
-        console.log('PAYLOAD RECIBIDO:', JSON.stringify(message));
-        console.log('TIPO:', typeof message.body);                
-        wapp
-          .sendMessage(
-            message.to,
-            { text: message.body }, // <-- ACA EL CAMBIO
-            message.options
-          )
-          .then(() => {
-            res.send("OK");
-            logger.debug("Message successfully sended from addon.");
-          })
-          .catch((error) => {
-            res.send("KO");
-            logger.error(error.message);
-          });
-      } else {
-        logger.error("Error in sending message. Client ID not found.");
-        res.send("KO");
-      }
-    } else {
-      logger.error("Error in sending message. Please specify client ID.");
-      res.send("KO");
-    }
-  });
-
-  app.post("/setStatus", (req, res) => {
-    const status = req.body.status;
-    if (req.body.hasOwnProperty("clientId")) {
-      if (clients.hasOwnProperty(req.body.clientId)) {
-        const wapp = clients[req.body.clientId];
-
-        wapp
-          .updateProfileStatus(status)
-          .then(() => {
-            res.send("OK");
-          })
-          .catch((error) => {
-            res.send("KO");
-            logger.error(error.message);
-          });
-      } else {
-        logger.error("Error in set status. Client ID not found.");
-        res.send("KO");
-      }
-    } else {
-      logger.error("Error in set status. Please specify client ID.");
-      res.send("KO");
-    }
-  });
-
-  app.post("/presenceSubscribe", (req, res) => {
-    const request = req.body;
-
-    if (req.body.hasOwnProperty("clientId")) {
-      if (clients.hasOwnProperty(req.body.clientId)) {
-        const wapp = clients[req.body.clientId];
-
-        wapp
-          .presenceSubscribe(request.userId)
-          .then(() => {
-            res.send("OK");
-          })
-          .catch((error) => {
-            res.send("KO");
-            logger.error(error.message);
-          });
-      } else {
-        logger.error("Error in subscribe presence. Client ID not found.");
-        res.send("KO");
-      }
-    } else {
-      logger.error("Error in subscribe presence. Please specify client ID.");
-      res.send("KO");
-    }
-  });
-
-  app.post("/sendPresenceUpdate", (req, res) => {
-    const request = req.body;
-
-    if (req.body.hasOwnProperty("clientId")) {
-      if (clients.hasOwnProperty(req.body.clientId)) {
-        const wapp = clients[req.body.clientId];
-
-        wapp
-          .sendPresenceUpdate(request.type, request.to)
-          .then(() => {
-            res.send("OK");
-          })
-          .catch((error) => {
-            res.send("KO");
-            logger.error(error.message);
-          });
-      } else {
-        logger.error("Error in presence update. Client ID not found.");
-        res.send("KO");
-      }
-    } else {
-      logger.error("Error in presence update. Please specify client ID.");
-      res.send("KO");
-    }
-  });
-
-  app.post("/sendInfinityPresenceUpdate", (req, res) => {
-    const request = req.body;
-
-    if (req.body.hasOwnProperty("clientId")) {
-      if (clients.hasOwnProperty(req.body.clientId)) {
-        const wapp = clients[req.body.clientId];
-
-        wapp
-          .setSendPresenceUpdateInterval(request.type, request.to)
-          .then(() => {
-            res.send("OK");
-          })
-          .catch((error) => {
-            res.send("KO");
-            logger.error(error.message);
-          });
-      } else {
-        logger.error("Error in presence update. Client ID not found.");
-        res.send("KO");
-      }
-    } else {
-      logger.error("Error in presence update. Please specify client ID.");
-      res.send("KO");
-    }
-  });
+app.listen(3000, () => {
+  console.log("WhatsApp Proxy Addon corriendo en puerto 3000");
 });
