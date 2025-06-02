3b97957d9343f2d5f93ebdb1fb53434689375958
arormoser <arormoser@gmail.com>
2025-06-02 16:33:38 -0300

    Update whatsapp.js

--- whatsapp_addon/whatsapp.js
+++ whatsapp_addon/whatsapp.js
@@ -146,11 +146,7 @@ class WhatsappClient extends EventEmitter {
       if (msg.hasOwnProperty("message") && !msg.key.fromMe) {
         delete msg.message.messageContextInfo;
         const messageType = Object.keys(msg.message)[0];
-        this.emit("msg", {
-          type: messageType,
-          payload: { text: payloadText },
-          ...msg
-        });
+        this.emit("msg", { type: messageType, ...msg });
       }
     });
 
